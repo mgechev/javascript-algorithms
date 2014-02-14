4e820cc8997934b6ca0176cf10d97e24d6be2595
mgechev <mgechev@gmail.com>
2014-02-14 10:45:28 +0200

    Adds comparator for bubble sort

--- src/sorting/bubblesort/bubblesort.js
+++ src/sorting/bubblesort/bubblesort.js
@@ -1,4 +1,9 @@
 (function (exports) {
+
+  function comparator(a, b) {
+    return a - b;
+  }
+
   /**
    * The bubblesort algorithm. Complexity O(n^2).
    *
@@ -6,11 +11,12 @@
    * @param {array} array Input array
    * @returns {array} array Sorted array
    */
-  function bubbleSort(array) {
+  function bubbleSort(array, cmp) {
+    cmp = cmp || comparator;
     var temp;
     for (var i = 0; i < array.length; i += 1) {
       for (var j = i; j > 0; j -= 1) {
-        if (array[j] < array[j - 1]) {
+        if (cmp(array[j], array[j - 1]) < 0) {
           temp = array[j];
           array[j] = array[j - 1];
           array[j - 1] = temp;
--- test/bubblesort.spec.js
+++ test/bubblesort.spec.js
@@ -2,6 +2,19 @@ var bubbleSort = require('../src/sorting/bubblesort/bubblesort.js')
 
 describe('Bubble sort', function () {
 
+  function createRandomArray(options) {
+    options = options || {};
+    var size = options.size || 100,
+        precision = options.precision || 2,
+        multiplier = options.multiplier || 100;
+
+    var result = [];
+    for (var i = size; i > 0; i -= 1) {
+      result.push(parseFloat((Math.random() * multiplier).toFixed(precision)));
+    }
+    return result;
+  }
+
   it('should work with empty array', function () {
     expect(bubbleSort([])).toEqual([]);
   });
@@ -12,24 +25,24 @@ describe('Bubble sort', function () {
 
   it('should work with random non-sorted arrays', function () {
 
-    function createRandomArray(options) {
-      options = options || {};
-      var size = options.size || 100,
-          precision = options.precision || 2,
-          multiplier = options.multiplier || 100;
-
-      var result = [];
-      for (var i = size; i > 0; i -= 1) {
-        result.push(parseFloat((Math.random() * multiplier).toFixed(precision)));
-      }
-      return result;
-    }
-
     var array = createRandomArray();
     bubbleSort(array);
 
     for (var i = 0; i < array.length - 1; i += 1) {
       expect(array[i] <= array[i + 1]).toBeTruthy();
+    }
+  });
+
+  it('should sort the numbers in descending order when such comparator is provided', function () {
+    function comparator(a, b) {
+      return b - a;
+    }
+
+    var array = createRandomArray();
+    bubbleSort(array, comparator);
+
+    for (var i = 0; i < array.length - 1; i += 1) {
+      expect(array[i] >= array[i + 1]).toBeTruthy();
     }
   });
 
