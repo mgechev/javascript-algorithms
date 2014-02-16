3a77e0ee89cfe0af7e485cfbdd40b2ab4d94826e
mgechev <mgechev@gmail.com>
2014-02-16 10:26:32 +0200

    Adds comparator to heap sort

--- src/sorting/heapsort/heapsort.js
+++ src/sorting/heapsort/heapsort.js
@@ -1,5 +1,9 @@
 (function (exports) {
 
+  function comparator(a, b) {
+    return a - b;
+  }
+
   /**
    * The heapsort algorithm. It's complexity is O(nlog n).
    *
@@ -14,22 +18,22 @@
      * @param {array} array Array
      * @param {number} index Index of the element which palce in the max heap should be found.
      */
-    function heapify(array, index, heapSize) {
+    function heapify(array, index, heapSize, cmp) {
       var left = 2 * index + 1,
           right = 2 * index + 2,
           largest = index;
 
-      if (left < heapSize && array[left] > array[index])
+      if (left < heapSize && cmp(array[left], array[index]) > 0)
         largest = left;
 
-      if (right < heapSize && array[right] > array[largest])
+      if (right < heapSize && cmp(array[right], array[largest]) > 0)
         largest = right;
 
       if (largest !== index) {
         var temp = array[index];
         array[index] = array[largest];
         array[largest] = temp;
-        heapify(array, largest, heapSize);
+        heapify(array, largest, heapSize, cmp);
       }
     }
 
@@ -40,9 +44,9 @@
      * @param {array} array Array which should be turned into max heap
      * @returns {array} array Array turned into max heap
      */
-    function buildMaxHeap(array) {
+    function buildMaxHeap(array, cmp) {
       for (var i = Math.floor(array.length / 2); i >= 0; i -= 1) {
-        heapify(array, i, array.length);
+        heapify(array, i, array.length, cmp);
       }
       return array;
     }
@@ -54,16 +58,17 @@
      * @param {array} array Input array
      * @returns {array} array Sorted array
      */
-    return function (array) {
+    return function (array, cmp) {
+      cmp = cmp || comparator;
       var size = array.length,
           temp;
-      buildMaxHeap(array);
+      buildMaxHeap(array, cmp);
       for (var i = array.length - 1; i > 0; i -= 1) {
         temp = array[0];
         array[0] = array[i];
         array[i] = temp;
         size -= 1;
-        heapify(array, 0, size);
+        heapify(array, 0, size, cmp);
       }
       return array;
     };
