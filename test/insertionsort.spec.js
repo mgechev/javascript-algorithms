a253cf87ab975426b711173e99a44ac302e78d44
mgechev <mgechev@gmail.com>
2014-02-18 10:32:26 +0200

    Adds tests for insertion sort and heap sort

---
+++ test/insertionsort.spec.js
@@ -0,0 +1,4 @@
+var sortTestCase = require('./sort.testcase.js'),
+    insertionSort = require('../src/sorting/insertionsort/insertionsort.js').insertionSort;
+
+sortTestCase(insertionSort, 'Insertion sort');
