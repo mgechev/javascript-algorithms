cc50184ebd9bb4c0296a5f1609f92c49aff3afd9
mgechev <mgechev@gmail.com>
2014-02-18 10:23:14 +0200

    Fixes indentation in insertion sort

--- src/sorting/insertionsort/insertionsort.js
+++ src/sorting/insertionsort/insertionsort.js
@@ -1,5 +1,3 @@
-var array = [2,3,5,1,2,4,7,9,0,3,3];
-
 /**
  * Insertionsort algorithm. It's complexity is O(n^2).
  *
@@ -8,18 +6,16 @@ var array = [2,3,5,1,2,4,7,9,0,3,3];
  * @returns {array} array Sorted array
  */
 function insertionSort(array) {
-    var current,
-        j;
-    for (var i = 1; i < array.length; i += 1) {
-        current = array[i];
-        j = i - 1;
-        while (j >= 0 && array[j] > current) {
-            array[j + 1] = array[j];
-            j -= 1;
-        }
-        array[j + 1] = current;
+  var current,
+      j;
+  for (var i = 1; i < array.length; i += 1) {
+    current = array[i];
+    j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j -= 1;
     }
-    return array;
+    array[j + 1] = current;
+  }
+  return array;
 }
-
-console.log(insertionSort(array));
