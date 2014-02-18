c4edde30ae427091164efd300419290591359e45
mgechev <mgechev@gmail.com>
2014-02-18 10:32:10 +0200

    Exports insertion sort and adds compare callback

--- src/sorting/insertionsort/insertionsort.js
+++ src/sorting/insertionsort/insertionsort.js
@@ -1,21 +1,34 @@
-/**
- * Insertionsort algorithm. It's complexity is O(n^2).
- *
- * @public
- * @param {array} array Input array
- * @returns {array} array Sorted array
- */
-function insertionSort(array) {
-  var current,
-      j;
-  for (var i = 1; i < array.length; i += 1) {
-    current = array[i];
-    j = i - 1;
-    while (j >= 0 && array[j] > current) {
-      array[j + 1] = array[j];
-      j -= 1;
-    }
-    array[j + 1] = current;
+(function (exports) {
+
+  'use strict';
+
+  function compare(a, b) {
+    return a - b;
   }
-  return array;
-}
+
+  /**
+   * Insertionsort algorithm. It's complexity is O(n^2).
+   *
+   * @public
+   * @param {array} array Input array
+   * @returns {array} array Sorted array
+   */
+  function insertionSort(array, cmp) {
+    cmp = cmp || compare;
+    var current,
+        j;
+    for (var i = 1; i < array.length; i += 1) {
+      current = array[i];
+      j = i - 1;
+      while (j >= 0 && cmp(array[j], current) > 0) {
+        array[j + 1] = array[j];
+        j -= 1;
+      }
+      array[j + 1] = current;
+    }
+    return array;
+  }
+
+  exports.insertionSort = insertionSort;
+
+}(typeof exports === 'undefined' ? window : exports));
