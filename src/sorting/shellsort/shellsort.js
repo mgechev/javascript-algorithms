34676b3eb0bd65d20038f84f61713edb4310d9f5
mgechev <mgechev@gmail.com>
2014-02-23 12:51:10 +0200

    Changes the indentation in shellsort

--- src/sorting/shellsort/shellsort.js
+++ src/sorting/shellsort/shellsort.js
@@ -1,10 +1,12 @@
-/**
- * Shellsort
- *
- * Shellsort uses the gaps 701, 301, 132, 57, 23, 10, 4, 1 and uses insertion sort
- * to sort the sub-arrays which match for the different gaps.
- */
-var shellsort = (function () {
+(function (exports) {
+
+  /**
+   * Shellsort
+   *
+   * Shellsort uses the gaps 701, 301, 132, 57, 23, 10, 4, 1 and uses insertion sort
+   * to sort the sub-arrays which match for the different gaps.
+   */
+  var shellSort = (function () {
 
     var gaps = [701, 301, 132, 57, 23, 10, 4, 1];
 
@@ -16,19 +18,23 @@ var shellsort = (function () {
      * @return {array} Sorted array
      */
     return function (array) {
-        var gap, current;
+      var gap, current;
 
-        for (var k = 0; k < gaps.length; k += 1) {
-            gap = gaps[k];
-            for (var i = gap; i < array.length; i += gap) {
-                current = array[i];
-                for (var j = i; j >= gap && array[j - gap] > current; j -= gap) {
-                    array[j] = array[j - gap];
-                }
-                array[j] = current;
-            }
+      for (var k = 0; k < gaps.length; k += 1) {
+        gap = gaps[k];
+        for (var i = gap; i < array.length; i += gap) {
+          current = array[i];
+          for (var j = i; j >= gap && array[j - gap] > current; j -= gap) {
+            array[j] = array[j - gap];
+          }
+          array[j] = current;
         }
-        return array;
+      }
+      return array;
     };
 
-}());
+  }());
+
+  exports.shellSort = shellSort;
+
+}(typeof exports === 'undefined' ? window : exports));
