276ede0c528acaaefecf8a9cdbc15bf0f3e67980
mgechev <mgechev@gmail.com>
2014-02-27 10:22:42 +0200

    Add tests for selection sort

---
+++ test/selectionsort.spec.js
@@ -0,0 +1,5 @@
+var sortTestCase = require('./sort.testcase.js'),
+    selectionSort = require('../src/sorting/selectionsort/selectionsort.js')
+      .selectionSort;
+
+sortTestCase(selectionSort, 'Selection sort');
