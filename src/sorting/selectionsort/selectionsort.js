55824fcbf7d55a0ec76163801f1baffe00004a53
mgechev <mgechev@gmail.com>
2014-02-27 10:19:43 +0200

    Change indentation in selection sort

--- src/sorting/selectionsort/selectionsort.js
+++ src/sorting/selectionsort/selectionsort.js
@@ -6,20 +6,19 @@
  * @return {array} The sorted array
  */
 function selectionSort(array) {
-    var min, idx, temp;
-    for (var i = 0; i < array.length; i += 1) {
-        idx = i;
-        min = array[i];
-        for (var j = i + 1; j < array.length; j += 1) {
-            if (min > array[j]) {
-                min = array[j];
-                idx = j;
-            }
-        }
-        temp = array[i];
-        array[i] = min;
-        array[idx] = temp;
+  var min, idx, temp;
+  for (var i = 0; i < array.length; i += 1) {
+    idx = i;
+    min = array[i];
+    for (var j = i + 1; j < array.length; j += 1) {
+      if (min > array[j]) {
+        min = array[j];
+        idx = j;
+      }
     }
-    return array;
+    temp = array[i];
+    array[i] = min;
+    array[idx] = temp;
+  }
+  return array;
 }
-
