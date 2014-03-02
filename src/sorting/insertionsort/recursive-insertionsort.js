115f607f45ace039c4c202aff4e562d612e25d4f
mgechev <mgechev@gmail.com>
2014-03-02 12:46:15 +0200

    Fix indentation

--- src/sorting/insertionsort/recursive-insertionsort.js
+++ src/sorting/insertionsort/recursive-insertionsort.js
@@ -3,16 +3,21 @@
  *
  * @public
  * @param {array} array Input array
- * @param {number} [max] Index of the element which place we should find in the current function call
+ * @param {number} [max] Index of the element which place we should find
+ *                       in the current function call
  */
 function recursiveInsertionSort(array, max) {
-    if (max <= 0)
-        return array;
-    if (max === undefined)
-        max = array.length - 1;
-    recursiveInsertionSort(array, max - 1);
-    for (var i = max - 1, current = array[max]; i >= 0 && current < array[i]; i -= 1)
-        array[i + 1] = array[i];
-    array[i + 1] = current;
+  if (max <= 0) {
     return array;
+  }
+  if (max === undefined) {
+    max = array.length - 1;
+  }
+  recursiveInsertionSort(array, max - 1);
+  for (var i = max - 1, current = array[max];
+      i >= 0 && current < array[i]; i -= 1) {
+    array[i + 1] = array[i];
+  }
+  array[i + 1] = current;
+  return array;
 }
