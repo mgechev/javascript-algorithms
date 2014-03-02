124eba27e8d2fade8d5a861967de9f5d80b23f99
mgechev <mgechev@gmail.com>
2014-03-02 15:37:07 +0200

    Wrap in IIFE + use strict

--- src/sorting/insertionsort/recursive-insertionsort.js
+++ src/sorting/insertionsort/recursive-insertionsort.js
@@ -1,23 +1,31 @@
-/**
- * Recursive version of insertionsort. Complexity O(n^2).
- *
- * @public
- * @param {array} array Input array
- * @param {number} [max] Index of the element which place we should find
- *                       in the current function call
- */
-function recursiveInsertionSort(array, max) {
-  if (max <= 0) {
+(function (exports) {
+
+  'use strict';
+
+  /**
+   * Recursive version of insertionsort. Complexity O(n^2).
+   *
+   * @public
+   * @param {array} array Input array
+   * @param {number} [max] Index of the element which place we should find
+   *                       in the current function call
+   */
+  function recursiveInsertionSort(array, max) {
+    if (max <= 0) {
+      return array;
+    }
+    if (max === undefined) {
+      max = array.length - 1;
+    }
+    recursiveInsertionSort(array, max - 1);
+    for (var i = max - 1, current = array[max];
+        i >= 0 && current < array[i]; i -= 1) {
+      array[i + 1] = array[i];
+    }
+    array[i + 1] = current;
     return array;
   }
-  if (max === undefined) {
-    max = array.length - 1;
-  }
-  recursiveInsertionSort(array, max - 1);
-  for (var i = max - 1, current = array[max];
-      i >= 0 && current < array[i]; i -= 1) {
-    array[i + 1] = array[i];
-  }
-  array[i + 1] = current;
-  return array;
-}
+
+  exports.recursiveInsertionSort = recursiveInsertionSort;
+
+}(typeof exports === 'undefined' ? window : exports));
