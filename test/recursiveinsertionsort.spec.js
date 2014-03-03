9022cd811c3f083e800a3b7248e3b059a6692bcf
mgechev <mgechev@gmail.com>
2014-03-03 10:40:36 +0200

    Add tests for the recursive insertion sort

---
+++ test/recursiveinsertionsort.spec.js
@@ -0,0 +1,4 @@
+var sortTestCase = require('./sort.testcase.js'),
+    recursiveInsertionSort = require('../src/sorting/insertionsort/recursive-insertionsort.js').recursiveInsertionSort;
+
+sortTestCase(recursiveInsertionSort, 'Recursive insertion sort');
