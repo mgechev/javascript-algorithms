4d8d21fa3ee81e0f5683805ae1fbf774877c4d56
mgechev <mgechev@gmail.com>
2014-03-03 10:40:06 +0200

    Add compare callback

--- src/sorting/insertionsort/recursive-insertionsort.js
+++ src/sorting/insertionsort/recursive-insertionsort.js
@@ -2,6 +2,10 @@
 
   'use strict';
 
+  function compare(a, b) {
+    return a - b;
+  }
+
   /**
    * Recursive version of insertionsort. Complexity O(n^2).
    *
@@ -10,16 +14,17 @@
    * @param {number} [max] Index of the element which place we should find
    *                       in the current function call
    */
-  function recursiveInsertionSort(array, max) {
+  function recursiveInsertionSort(array, cmp, max) {
+    cmp = cmp || compare;
     if (max <= 0) {
       return array;
     }
     if (max === undefined) {
       max = array.length - 1;
     }
-    recursiveInsertionSort(array, max - 1);
+    recursiveInsertionSort(array, cmp, max - 1);
     for (var i = max - 1, current = array[max];
-        i >= 0 && current < array[i]; i -= 1) {
+        i >= 0 && cmp(current, array[i]) < 0; i -= 1) {
       array[i + 1] = array[i];
     }
     array[i + 1] = current;
