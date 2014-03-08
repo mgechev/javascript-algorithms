370e8c7b44a64bbd2319d0ffdcc1c24256410f52
mgechev <mgechev@gmail.com>
2014-03-08 16:25:30 +0200

    Fix the indentation of bucketsort

--- src/sorting/linearsort/bucketsort.js
+++ src/sorting/linearsort/bucketsort.js
@@ -1,5 +1,3 @@
-var array = [0.21, 0.44, 0.221, 0.01, 0.88];
-
 /**
  * Bucketsort. This algorithm has complexity O(n) but it's not
  * correct for every input.
@@ -8,100 +6,99 @@ var array = [0.21, 0.44, 0.221, 0.01, 0.88];
  */
 var bucketSort = (function () {
 
-    /**
-     * Insertionsort.
-     *
-     * @private
-     * @param {array} array Input array
-     * @returns {array} array Sorted input array
-     */
-    function insertionSort(array) {
-        var current,
-            j;
-        for (var i = 1; i < array.length; i += 1) {
-            current = array[i];
-            j = i - 1;
-            while (j >= 0 && current < array[j]) {
-                array[j + 1] = array[j];
-                j -= 1;
-            }
-            array[j + 1] = current;
-        }
-        return array;
+  /**
+   * Insertionsort.
+   *
+   * @private
+   * @param {array} array Input array
+   * @returns {array} array Sorted input array
+   */
+  function insertionSort(array) {
+    var current,
+        j;
+    for (var i = 1; i < array.length; i += 1) {
+      current = array[i];
+      j = i - 1;
+      while (j >= 0 && current < array[j]) {
+        array[j + 1] = array[j];
+        j -= 1;
+      }
+      array[j + 1] = current;
     }
+    return array;
+  }
 
-    /**
-     * Creates buckets for given array
-     *
-     * @private
-     * @param {array} array Input array
-     * @returns {array} buckets Array whith array for each bucket. 
-     *  Each bucket contains an array with all elements from the input which are with suitable size.
-     */
-    function createBuckets(array) {
-        var buckets = [],
-            currentBucket,
-            current,
-            sectorSize = 1 / array.length;
-        for (var i = 0; i < array.length; i += 1) {
-            current = array[i];
-            currentBucket = Math.floor(current / sectorSize);
-            if (buckets[currentBucket] === undefined) {
-                buckets[currentBucket] = [];
-            }
-            buckets[currentBucket].push(current);
-        }
-        return buckets;
+  /**
+   * Creates buckets for given array
+   *
+   * @private
+   * @param {array} array Input array
+   * @returns {array} buckets Array whith array for each bucket.
+   *  Each bucket contains an array with all elements from the input which are with suitable size.
+   */
+  function createBuckets(array) {
+    var buckets = [],
+        currentBucket,
+        current,
+        sectorSize = 1 / array.length;
+    for (var i = 0; i < array.length; i += 1) {
+      current = array[i];
+      currentBucket = Math.floor(current / sectorSize);
+      if (buckets[currentBucket] === undefined) {
+        buckets[currentBucket] = [];
+      }
+      buckets[currentBucket].push(current);
     }
+    return buckets;
+  }
 
-    /**
-     * Sorts the arrays from each bucket.
-     *
-     * @private
-     * @param {array} buckets Given buckets
-     * @returns {array} buckets Buckets with sorted arrays for each bucket
-     */
-    function sortBuckets(buckets) {
-        for (var i = 0; i < buckets.length; i += 1) {
-            if (buckets[i] !== undefined)
-                insertionSort(buckets[i]);
-        }
-        return buckets;
+  /**
+   * Sorts the arrays from each bucket.
+   *
+   * @private
+   * @param {array} buckets Given buckets
+   * @returns {array} buckets Buckets with sorted arrays for each bucket
+   */
+  function sortBuckets(buckets) {
+    for (var i = 0; i < buckets.length; i += 1) {
+      if (buckets[i] !== undefined) {
+        insertionSort(buckets[i]);
+      }
     }
+    return buckets;
+  }
 
-    /**
-     * Unions all buckets' arrays
-     *
-     * @private
-     * @param {array} buckets Input buckets
-     * @returns {array} result Sorted array which contains all elements form each bucket
-     */
-    function unionBuckets(buckets) {
-        var result = [],
-            currentBucket;
-        for (var i = 0; i < buckets.length; i += 1) {
-            currentBucket = buckets[i];
-            if (currentBucket !== undefined) {
-                for (var j = 0; j < currentBucket.length; j += 1) {
-                    result.push(currentBucket[j]);
-                }
-            }
+  /**
+   * Unions all buckets' arrays
+   *
+   * @private
+   * @param {array} buckets Input buckets
+   * @returns {array} result Sorted array which contains all elements form each bucket
+   */
+  function unionBuckets(buckets) {
+    var result = [],
+        currentBucket;
+    for (var i = 0; i < buckets.length; i += 1) {
+      currentBucket = buckets[i];
+      if (currentBucket !== undefined) {
+        for (var j = 0; j < currentBucket.length; j += 1) {
+          result.push(currentBucket[j]);
         }
-        return result;
+      }
     }
+    return result;
+  }
 
-    /**
-     * Sorts given array with bucketsort
-     *
-     * @public
-     * @param {array} array Input array which should be sorted
-     * @returns {array} Sorted array
-     */
-    return function (array) {
-        var buckets = createBuckets(array);
-        sortBuckets(buckets);
-        return unionBuckets(buckets);
-    };
+  /**
+   * Sorts given array with bucketsort
+   *
+   * @public
+   * @param {array} array Input array which should be sorted
+   * @returns {array} Sorted array
+   */
+  return function (array) {
+    var buckets = createBuckets(array);
+    sortBuckets(buckets);
+    return unionBuckets(buckets);
+  };
 }());
-
-console.log(bucketSort(array));
