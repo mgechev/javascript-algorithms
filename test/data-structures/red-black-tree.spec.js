c0eb3515933335eeffdc4a781ae353bfb3489255
mgechev <mgechev@gmail.com>
2014-05-15 05:10:39 -0700

    Add tests for Node

--- test/data-structures/red-black-tree.spec.js
+++ test/data-structures/red-black-tree.spec.js
@@ -7,4 +7,10 @@ describe('Node', function () {
   it('should be a constructor function', function () {
     expect(typeof Node).toBe('function');
   });
+  it('should set all properties via the constructor', function () {
+    var node = new Node('key', 'value', true);
+    expect(node.getKey()).toBe('key');
+    expect(node.getValue()).toBe('value');
+    expect(node.isRed()).toBeTruthy();
+  });
 });
