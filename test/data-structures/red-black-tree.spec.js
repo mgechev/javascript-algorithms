9a4588a73a5412d012f927ed7ea3993d45bf0b18
mgechev <mgechev@gmail.com>
2014-05-16 16:13:39 -0700

    Add tests

--- test/data-structures/red-black-tree.spec.js
+++ test/data-structures/red-black-tree.spec.js
@@ -5,9 +5,11 @@ var mod = require('../../src/data-structures/red-black-tree.js'),
     RBTree = mod.RBTree;
 
 describe('Node', function () {
+
   it('should be a constructor function', function () {
     expect(typeof Node).toBe('function');
   });
+
   it('should set all properties via the constructor', function () {
     var node = new Node('key', 'value', 1, 2, true);
     expect(node.getKey()).toBe('key');
@@ -19,9 +21,20 @@ describe('Node', function () {
     //if we set isRed to falcy it should be turned to red
     expect(node.isRed()).toBe(false);
   });
-  it('should has method flipColor', function () {
-    var node = new Node();
-    expect(typeof node.flipColor).toBe('function');
+
+  describe('Node flipColor', function () {
+    it('should has method flipColor', function () {
+      var node = new Node();
+      expect(typeof node.flipColor).toBe('function');
+    });
+    it('should work properly', function () {
+      var node = new Node();
+      expect(node.isRed()).toBe(false);
+      node.flipColor();
+      expect(node.isRed()).toBe(true);
+      node.flipColor();
+      expect(node.isRed()).toBe(false);
+    });
   });
 });
 
