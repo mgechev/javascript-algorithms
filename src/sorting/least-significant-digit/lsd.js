b96fb337711eedf11a59945911aa1248c65af6f3
mgechev <mgechev@gmail.com>
2014-09-23 22:16:13 +0300

    Update lsd

--- src/sorting/least-significant-digit/lsd.js
+++ src/sorting/least-significant-digit/lsd.js
@@ -1,34 +1,47 @@
-function lsd(arr, letterIdx) {
+(function (exports) {
   'use strict';
-  var temp, count, max;
-  letterIdx = letterIdx || 1;
-  for (var i = letterIdx - 1; i >= 0; i -= 1) {
-    count = [];
-    max = -Infinity;
-    temp = [];
-    for (var j = 0; j < arr.length; j += 1) {
-      var charCode = arr[j].charCodeAt(i);
-      if (max < charCode) {
-        max = charCode;
-      }
-      var old = count[charCode + 1] || 0;
-      count[charCode + 1] = old + 1;
-    }
-    for (var c = 0; c < max; c += 1) {
-      count[c] = count[c] || 0;
-      count[c + 1] = count[c + 1] || 0;
-      count[c + 1] += count[c];
-    }
-    for (j = 0; j < arr.length; j += 1) {
-      var code = arr[j].charCodeAt(i);
-      temp[count[code]] = arr[j];
-      count[code] += 1;
-    }
-    for (j = 0; j < arr.length; j += 1) {
-      arr[j] = temp[j];
-    }
-  }
-  return arr;
-}
 
-//module.exports.lsd = lsd;
+  /**
+   * Sorts strings lexicographically.
+   * Complexity O(n*m)
+   *
+   * @public
+   * @param {Array} arr Input array
+   * @param {Number} letterIdx Index to start sorting from
+   * @returns {Array} Sorted array
+   */
+  function lsd(arr, letterIdx) {
+    var temp, count, max;
+    letterIdx = letterIdx || 1;
+    for (var i = letterIdx - 1; i >= 0; i -= 1) {
+      count = [];
+      max = -Infinity;
+      temp = [];
+      for (var j = 0; j < arr.length; j += 1) {
+        var charCode = arr[j].charCodeAt(i);
+        if (max < charCode) {
+          max = charCode;
+        }
+        var old = count[charCode + 1] || 0;
+        count[charCode + 1] = old + 1;
+      }
+      for (var c = 0; c < max; c += 1) {
+        count[c] = count[c] || 0;
+        count[c + 1] = count[c + 1] || 0;
+        count[c + 1] += count[c];
+      }
+      for (j = 0; j < arr.length; j += 1) {
+        var code = arr[j].charCodeAt(i);
+        temp[count[code]] = arr[j];
+        count[code] += 1;
+      }
+      for (j = 0; j < arr.length; j += 1) {
+        arr[j] = temp[j];
+      }
+    }
+    return arr;
+  }
+
+  exports.lsd = lsd;
+
+}(typeof exports === 'undefined' ? window : exports));
