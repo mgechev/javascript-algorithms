c2c8203bf09a0c6c7f8c774b5bb0142971775cf7
mgechev <mgechev@gmail.com>
2014-10-07 11:32:37 +0300

    Update indentation in floyd

--- src/graphs/shortest-path/floyd.js
+++ src/graphs/shortest-path/floyd.js
@@ -19,56 +19,56 @@ var graph = [[NaN, 7,   9,   NaN, NaN, 16],
  */
 var floydWarshall = (function () {
 
-    /**
-     * Matrix used for the algorithm.
-     */
-    var dist;
+  /**
+   * Matrix used for the algorithm.
+   */
+  var dist;
 
-    /**
-     * Initialize the distance matrix
-     *
-     * @private
-     * @param {array} graph Distance matrix of the array
-     * @return {array} Distance matrix used for the algorithm
-     */
-    function init(graph) {
-        var dist = [];
-        var size = graph.length;
-        for (var i = 0; i < size; i += 1) {
-            dist[i] = [];
-            for (var j = 0; j < size; j += 1) {
-                if (i === j) {
-                    dist[i][j] = 0;
-                } else if (isNaN(graph[i][j])) {
-                    dist[i][j] = Infinity;
-                } else {
-                    dist[i][j] = graph[i][j];
-                }
-            }
+  /**
+   * Initialize the distance matrix
+   *
+   * @private
+   * @param {array} graph Distance matrix of the array
+   * @return {array} Distance matrix used for the algorithm
+   */
+  function init(graph) {
+    var dist = [];
+    var size = graph.length;
+    for (var i = 0; i < size; i += 1) {
+      dist[i] = [];
+      for (var j = 0; j < size; j += 1) {
+        if (i === j) {
+          dist[i][j] = 0;
+        } else if (isNaN(graph[i][j])) {
+          dist[i][j] = Infinity;
+        } else {
+          dist[i][j] = graph[i][j];
         }
-        return dist;
+      }
     }
+    return dist;
+  }
 
-    /**
-     * Finds the shortest path between each two vertices
-     * Complexity O(n^3)
-     *
-     * @public
-     * @param {array} graph The graph which should be processed
-     * @return {array} The array which contains the shortest distance between each two vertices
-     */
-    return function (graph) {
-        dist = init(graph);
-        var size = graph.length;
-        for (var k = 0; k < size; k += 1) {
-            for (var i = 0; i < size; i += 1) {
-                for (var j = 0; j < size; j += 1) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
+  /**
+   * Finds the shortest path between each two vertices
+   * Complexity O(n^3)
+   *
+   * @public
+   * @param {array} graph The graph which should be processed
+   * @return {array} The array which contains the shortest distance between each two vertices
+   */
+  return function (graph) {
+    dist = init(graph);
+    var size = graph.length;
+    for (var k = 0; k < size; k += 1) {
+      for (var i = 0; i < size; i += 1) {
+        for (var j = 0; j < size; j += 1) {
+          if (dist[i][j] > dist[i][k] + dist[k][j]) {
+            dist[i][j] = dist[i][k] + dist[k][j];
+          }
         }
-        return dist;
-    };
+      }
+    }
+    return dist;
+  };
 }());
