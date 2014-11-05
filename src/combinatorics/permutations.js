55050a8095558841b6a0914cb5c0783a1654e1be
mgechev <mgechev@gmail.com>
2014-11-05 16:11:08 +0200

    Exports the function

--- src/combinatorics/permutations.js
+++ src/combinatorics/permutations.js
@@ -1,28 +1,33 @@
-var permutations = (function () {
+(function (exports) {
   'use strict';
+  var permutations = (function () {
 
-  var res;
+    var res;
 
-  function swap(arr, i, j) {
-    var temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-
-  function permutations(arr, current) {
-    if (current >= arr.length) {
-      return res.push(arr.slice());
+    function swap(arr, i, j) {
+      var temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
     }
-    for (var i = current; i < arr.length; i += 1) {
-      swap(arr, i, current);
-      permutations(arr, current + 1);
-      swap(arr, i, current);
-    }
-  }
 
-  return function (arr) {
-    res = [];
-    permutations(arr, 0);
-    return res;
-  };
-}());
+    function permutations(arr, current) {
+      if (current >= arr.length) {
+        return res.push(arr.slice());
+      }
+      for (var i = current; i < arr.length; i += 1) {
+        swap(arr, i, current);
+        permutations(arr, current + 1);
+        swap(arr, i, current);
+      }
+    }
+
+    return function (arr) {
+      res = [];
+      permutations(arr, 0);
+      return res;
+    };
+  }());
+
+  exports.permutations = permutations;
+
+}(typeof exports === 'undefined' ? window : exports));
