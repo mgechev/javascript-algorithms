d33ee89dcd59d6e254561a9a3e9c847580037c39
mgechev <mgechev@gmail.com>
2014-11-18 17:16:28 +0200

    Update suffix tree

--- src/data-structures/suffix-tree.js
+++ src/data-structures/suffix-tree.js
@@ -90,3 +90,7 @@ function isSubstr(tree, str) {
 // var suffix = new SuffixTree();
 // suffix.build('banana');
 // console.log(suffix);
+
+var st = new SuffixTree();
+st.build('Since these cases are very common use cases, the problem was easily solved by choosing either a random index for the pivot, choosing the middle index of the partition or (especially for longer partitions) choosing the median of the first, middle and last element of the partition for the pivot. With these modifications, the worst case of Quick sort has less chances to occur, but worst case can still occur if the input array is such that the maximum (or minimum) element is always chosen as pivot.');
+console.log(JSON.stringify(st));
