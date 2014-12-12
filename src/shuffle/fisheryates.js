8a3c0a0f22caf0c87ca70277cfd09b774bfeacaf
mgechev <mgechev@gmail.com>
2014-12-12 15:20:08 +0000

    Export Fisher Yates shuffling algorithm

--- src/shuffle/fisheryates.js
+++ src/shuffle/fisheryates.js
@@ -1,20 +1,26 @@
-/**
- * The shuffling algorithm of
- * Fisher-Yates. Complexity O(n)
- *
- * @param {array} array The array which should be shuffled
- * @return {array} The shuffled array.
- */
-function shuffle(array) {
+(function (exports) {
   'use strict';
-  var size = array.length,
-    rand, temp;
-  for (var i = 1; i < size; i += 1) {
-    rand = Math.round(Math.random() * i);
-    temp = array[rand];
-    array[rand] = array[i];
-    array[i] = temp;
+
+  /**
+   * The shuffling algorithm of
+   * Fisher-Yates. Complexity O(n)
+   *
+   * @param {array} array The array which should be shuffled
+   * @return {array} The shuffled array.
+   */
+  function shuffle(array) {
+    var size = array.length,
+      rand, temp;
+    for (var i = 1; i < size; i += 1) {
+      rand = Math.round(Math.random() * i);
+      temp = array[rand];
+      array[rand] = array[i];
+      array[i] = temp;
+    }
+    return array;
   }
-  return array;
-}
+
+  exports.shuffle = shuffle;
+
+}(typeof exports === 'undefined' ? window : exports));
 
