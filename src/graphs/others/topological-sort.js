4a15260d25bbd5b1cda7093c1d2bad2e7e47ac05
mgechev <mgechev@gmail.com>
2014-12-13 12:36:40 +0000

    Export topological sort

--- src/graphs/others/topological-sort.js
+++ src/graphs/others/topological-sort.js
@@ -1,44 +1,39 @@
-var topologicalSort = (function () {
+(function (exports) {
   'use strict';
 
-  function topologicalSortHelper(node, visited, temp, graph, result) {
-    temp[node] = true;
-    var neighbors = graph[node];
-    for (var i = 0; i < neighbors.length; i += 1) {
-      var n = neighbors[i];
-      if (temp[n]) {
-        throw new Error('The graph is not a DAG');
+  var topologicalSort = (function () {
+
+    function topologicalSortHelper(node, visited, temp, graph, result) {
+      temp[node] = true;
+      var neighbors = graph[node];
+      for (var i = 0; i < neighbors.length; i += 1) {
+        var n = neighbors[i];
+        if (temp[n]) {
+          throw new Error('The graph is not a DAG');
+        }
+        if (!visited[n]) {
+          topologicalSortHelper(n, visited, temp, graph, result);
+        }
       }
-      if (!visited[n]) {
-        topologicalSortHelper(n, visited, temp, graph, result);
-      }
+      temp[node] = false;
+      visited[node] = true;
+      result.push(node);
     }
-    temp[node] = false;
-    visited[node] = true;
-    result.push(node);
-  }
 
-  return function (graph) {
-    var result = [],
-        visited = [],
-        temp = [];
-    for (var node in graph) {
-      if (!visited[node] && !temp[node]) {
-        topologicalSortHelper(node, visited, temp, graph, result);
+    return function (graph) {
+      var result = [],
+          visited = [],
+          temp = [];
+      for (var node in graph) {
+        if (!visited[node] && !temp[node]) {
+          topologicalSortHelper(node, visited, temp, graph, result);
+        }
       }
-    }
-    return result.reverse();
-  };
-}());
+      return result.reverse();
+    };
+  }());
 
-var graph = {
-  '0': ['1', '2'],
-  '1': ['2', '4'],
-  '2': ['3'],
-  '3': [],
-  '4': ['5', '6'],
-  '5': [],
-  '6': ['3']
-};
+  exports.topologicalSort = topologicalSort;
 
-console.log(topologicalSort(graph));
+}(typeof exports === 'undefined' ? window : exports));
+
