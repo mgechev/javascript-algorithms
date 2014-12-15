c4608a4502e397b69bd5b1563bf9d1f6dd570a5b
mgechev <mgechev@gmail.com>
2014-12-15 17:49:20 +0200

    Wrap shuffle and export it

--- src/shuffle/richarddurstenfeld.js
+++ src/shuffle/richarddurstenfeld.js
@@ -1,24 +1,30 @@
-/**
- * Shuffle of an array elements.
- * This algorithm is modified version of Fisher-Yates shuffle
- * algorithm and is introduced by Richard Durstenfeld.
- */
-
-/**
- * Shuffles an array. Complexity O(n).
- *
- * @param {array} array An array which should be shuffled
- * @returns {array} Shuffled array
- */
-function shuffle(array) {
+(function (exports) {
   'use strict';
-  var arraySize = array.length - 1,
-      rand, temp;
-  for (var i = arraySize; i >= 0; i -= 1) {
-    rand = Math.round(Math.random() * arraySize);
-    temp = array[i];
-    array[i] = array[rand];
-    array[rand] = temp;
+  /**
+   * Shuffle of an array elements.
+   * This algorithm is modified version of Fisher-Yates shuffle
+   * algorithm and is introduced by Richard Durstenfeld.
+   */
+
+  /**
+   * Shuffles an array. Complexity O(n).
+   *
+   * @param {array} array An array which should be shuffled
+   * @returns {array} Shuffled array
+   */
+  function shuffle(array) {
+    var arraySize = array.length - 1,
+        rand, temp;
+    for (var i = arraySize; i >= 0; i -= 1) {
+      rand = Math.round(Math.random() * arraySize);
+      temp = array[i];
+      array[i] = array[rand];
+      array[rand] = temp;
+    }
+    return array;
   }
-  return array;
-}
+
+  exports.shuffle = shuffle;
+
+}(typeof exports === 'undefined' ? window : exports));
+
