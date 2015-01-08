942504f793fcd9cb970ea666d64ef6965f880acb
mgechev <mgechev@gmail.com>
2015-01-08 14:20:11 +0200

    Update dfs tests

--- test/graphs/searching/dfs.spec.js
+++ test/graphs/searching/dfs.spec.js
@@ -1,58 +1,37 @@
 'use strict';
 
-var sampleGraph = [[1, 1, 1, 0, 0, 0],
-                   [0, 1, 1, 1, 0, 0],
-                   [1, 0, 1, 1, 1, 0],
-                   [0, 1, 0, 1, 1, 0],
-                   [0, 1, 0, 1, 1, 0],
-                   [0, 1, 0, 1, 1, 0],
-                   [0, 0, 1, 1, 1, 1],
-                   [0, 0, 0, 0, 1, 1]];
-
-var dfs = require('../../../src/graphs/searching/dfs').depthFirstSearch;
+var dfs = require('../../../src/graphs/searching/dfs').dfs;
 
 describe('dfs', function () {
 
-  it('should work with incorrect input', function () {
-    expect(function () {
-      dfs(null, [1, 1], [1, 1]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [-1, -1], [0, 0]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [0, -1], [-1, 0]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [0, 0], [-1, 0]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [0, 1000], [-1, 0]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [100000, 1000], [-1, 0]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [0, 0], [100, 100]);
-    }).toThrow();
-    expect(function () {
-      dfs(sampleGraph, [0, 0], [5, 5]);
-    }).not.toThrow();
+  it('should work with empty graph', function () {
+    expect(dfs([[]])).toBeTruthy();
   });
 
-  it('should work with 1x1 matrix', function () {
-    var graph = [[1]];
-    expect(dfs(graph, [0, 0], [0, 0])).toBeTruthy();
-    graph = [[0]];
-    expect(dfs(graph, [0, 0], [0, 0])).toBeFalsy();
+  it('should always find a path between node and itself', function () {
+    expect(dfs([[0]]), 0, 0).toBeTruthy();
   });
 
-  it('should work in the general case', function () {
-    expect(dfs(sampleGraph, [0, 0], [1, 1])).toBeTruthy();
-    expect(dfs(sampleGraph, [0, 0], [6, 5])).toBeTruthy();
-    expect(dfs(sampleGraph, [0, 0], [0, 5])).toBeFalsy();
-    expect(dfs(sampleGraph, [1, 1], [6, 5])).toBeTruthy();
-    expect(dfs(sampleGraph, [1, 1], [0, 5])).toBeFalsy();
+  it('should always find a path between two directly connected nodes',
+    function () {
+      expect(dfs([[0, 1], [1, 0]], 0, 1)).toBeTruthy();
+      expect(dfs([[0, 1], [1, 0]], 1, 0)).toBeTruthy();
+    });
+
+  it('should always find a path between two directly connected' +
+    'connected nodes in a directed graph', function () {
+    expect(dfs([[0, 0], [1, 0]], 1, 0)).toBeTruthy();
   });
+
+  it('should always find a path between two indirectly connected nodes',
+    function () {
+      expect(dfs([[0, 1, 0], [0, 0, 1], [0, 0, 0]], 0, 2)).toBeTruthy();
+    });
+
+  it('should not find a path between two nodes, which are not connected',
+      function () {
+      expect(dfs([[0, 0], [1, 0]], 0, 1)).toBeFalsy();
+      expect(dfs([[0, 0, 0], [0, 0, 1], [0, 0, 0]], 0, 2)).toBeFalsy();
+    });
 
 });
