f48057148114a2f2059066c3fcbdeb86d620bb6b
mgechev <mgechev@gmail.com>
2015-01-10 12:13:43 +0200

    Fix the indentation according to jshintrc

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var gulp = require('gulp'),
     shell = require('gulp-shell');
 
 gulp.task('jsdoc', shell.task([
-    './node_modules/.bin/jsdoc -c ./doc-config.json',
+  './node_modules/.bin/jsdoc -c ./doc-config.json',
 ]));
