865abc46bad761b13210845f11f00c6dfe9db298
mgechev <mgechev@gmail.com>
2015-01-10 17:40:43 +0200

    Fix jshint config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
     jshint = require('gulp-jshint'),
-    jasmine = require('gulp-jasmine');
+    jasmine = require('gulp-jasmine'),
+    stylish = require('jshint-stylish');
 
 gulp.task('jsdoc', shell.task([
   './node_modules/.bin/jsdoc -c ./doc-config.json',
@@ -11,7 +12,8 @@ gulp.task('jsdoc', shell.task([
 gulp.task('lint', function () {
   return gulp.src(['./src/**/*.js'], ['./test/**/*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', function () {
