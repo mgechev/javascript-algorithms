fce486ec7d872c918771340d498294b826336ede
AndreyGeonya <a.geonya@gmail.com>
2015-01-13 01:09:01 +0200

    recursive bin search jsdoc

--- src/searching/binarysearch/recursive-binarysearch.js
+++ src/searching/binarysearch/recursive-binarysearch.js
@@ -1,49 +1,56 @@
 (function (exports) {
   'use strict';
-  /**
-   * Recursive version of binary search. It's complexity is O(log n).
-   *
-   * @public
-   */
+
   var binarySearch = (function () {
     /**
      * Binary search.
      *
      * @pivate
      * @param {array} array Array where we should find the index of the element
-     * @param {number} key Key of the element which index should be found
+     * @param {number} value Value of the element which index should be found
      * @param {number} left Left index
      * @param {number} right Right index
      * @returns {number} index The index of the element or -1 if not found
      *
      */
-    function recursiveBinarySearch(array, key, left, right) {
+    function recursiveBinarySearch(array, value, left, right) {
       if (left > right) {
         return -1;
       }
       var middle = Math.floor((right + left) / 2);
-      if (array[middle] === key) {
+      if (array[middle] === value) {
         return middle;
-      } else if (array[middle] > key) {
-        return recursiveBinarySearch(array, key, left, middle - 1);
+      } else if (array[middle] > value) {
+        return recursiveBinarySearch(array, value, left, middle - 1);
       } else {
-        return recursiveBinarySearch(array, key, middle + 1, right);
+        return recursiveBinarySearch(array, value, middle + 1, right);
       }
     }
 
     /**
-     * Calls the binary search function with it's initial values.
+     * Recursive version of binary search.
+     * Searchs for specific element in a given array using
+     * the binary search algorithm.<br><br>
+     * Time complexity: O(log N).
      *
-     * @param {array} array The input array
-     * @param {number} key The key of the element which index should be found
-     * @returns {number} index The index of the element or -1 if not found
+     * @example
+     *
+     * var search = require('path-to-algorithms/src/searching/'+
+     * 'binarysearch/recursive-binarysearch').binarySearch;
+     * console.log(search([1, 2, 3, 4, 5], 4)); // 3
+     *
+     * @public
+     * @module searching/binarysearch/recursive-binarysearch
+     * @param {Array} array Input array.
+     * @param {Number} value Value of the element which index should be found.
+     * @returns {Number} Index of the element or -1 if not found.
      */
-    return function (array, key) {
-      return recursiveBinarySearch(array, key, 0, array.length);
+    return function (array, value) {
+      return recursiveBinarySearch(array, value, 0, array.length);
     };
 
   }());
 
   exports.binarySearch = binarySearch;
 
-}(typeof exports === 'undefined' ? window : exports));
+})(typeof window === 'undefined' ? module.exports : window);
