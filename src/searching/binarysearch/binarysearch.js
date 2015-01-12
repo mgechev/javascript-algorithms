7be10e16da915f7cf5d03cb89d8fb6285b41d343
AndreyGeonya <a.geonya@gmail.com>
2015-01-13 01:02:20 +0200

    fix jshint error

--- src/searching/binarysearch/binarysearch.js
+++ src/searching/binarysearch/binarysearch.js
@@ -11,7 +11,7 @@
    * var search = require('path-to-algorithms/src/searching/'+
    * 'binarysearch/binarysearch').binarySearch;
    * console.log(search([1, 2, 3, 4, 5], 4)); // 3
-   * 
+   *
    * @public
    * @module searching/binarysearch/binarysearch
    * @param {Array} array Input array.
