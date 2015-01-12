ead628f93454325af9698da641af94465ca7230d
AndreyGeonya <a.geonya@gmail.com>
2015-01-12 15:44:05 +0200

    add livenshtein distance doc

--- src/others/levenshtein-distance.js
+++ src/others/levenshtein-distance.js
@@ -21,6 +21,26 @@
                       levenshteinDistance(s, ls - 1, t, lt - 1) + cost);
     }
 
+    /**
+     * The Levenshtein distance between two strings is a minimum number
+     * of edits needed to transform one string into the other, with the
+     * allowable edit operations being insertion, deletion,
+     * or substitution of a single character.
+     *
+     * @public
+     * @module others/levenshtein-distance
+     *
+     * @example
+     *
+     * var dist = require('path-to-algorithms/src/others/' +
+     * 'levenshtein-distance').levenshteinDistance;
+     * console.log(dist('kitten', 'sitting')); // 3
+     *
+     * @param {String} s Source string.
+     * @param {String} t Target string.
+     * @return {Number} Minimum number of edits needed
+     * to transform source string into the target string.
+     */
     return function (s, t) {
       return levenshteinDistance(s, s.length, t, t.length);
     };
