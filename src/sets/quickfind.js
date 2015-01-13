89f0caad3dbc1361745423b80be978baa901645e
AndreyGeonya <a.geonya@gmail.com>
2015-01-13 16:00:48 +0200

    quickfind jsdoc

--- src/sets/quickfind.js
+++ src/sets/quickfind.js
@@ -1,12 +1,33 @@
+/**
+ * Keeps track of a set of elements partitioned into a
+ * number of disjoint (nonoverlapping) subsets.
+ *
+ * @example
+ *
+ * var QuickFind = require('path-to-algorithms/src/sets/quickfind').QuickFind;
+ *
+ * var find = new QuickFind(10);
+ * find.union(0, 1);
+ * find.union(2, 1);
+ * find.union(3, 4);
+ * find.union(8, 9);
+ * find.union(4, 8);
+ *
+ * console.log(find.connected(0, 9)); // false
+ * console.log(find.connected(3, 9)); // true
+ *
+ * @public
+ * @module sets/quickfind
+ */
 (function (exports) {
   'use strict';
 
   /**
-   * Checks whether there is a path between two nodes.
-   * The initialization is O(n).
+   * Initialization.<br><br>
+   * Complexity: O(N).
    *
    * @constructor
-   * @param {numner} size The count of the nodes
+   * @param {Numner} size Count of the nodes.
    */
   function QuickFind(size) {
     this._ids = [];
@@ -16,11 +37,11 @@
   }
 
   /**
-   * Connects two nodes - p and q.
-   * Complexity O(n).
+   * Connects two nodes - p and q.<br><br>
+   * Complexity: O(N).
    *
-   * @param {number} p The first node
-   * @param {number} q The second node
+   * @param {Number} p The first node.
+   * @param {Number} q The second node.
    */
   QuickFind.prototype.union = function (p, q) {
     var size = this._ids.length;
@@ -34,11 +55,11 @@
   };
 
   /**
-   * Checks whether two nodes are connected.
-   * Complexity O(1).
+   * Checks whether two nodes are connected.<br><br>
+   * Complexity: O(1).
    *
-   * @param {number} p The first node
-   * @param {number} q The second node
+   * @param {number} p The first node.
+   * @param {number} q The second node.
    *
    */
   QuickFind.prototype.connected = function (p, q) {
@@ -47,13 +68,4 @@
 
   exports.QuickFind = QuickFind;
 
-  //var find = new QuickFind(10);
-  //find.union(0, 1);
-  //find.union(2, 1);
-  //find.union(3, 4);
-  //find.union(8, 9);
-  //find.union(4, 8);
-  //
-  //console.log(find.connected(0, 9)); //expected false
-  //console.log(find.connected(3, 9)); //expected true
-}(typeof exports === 'undefined' ? window : exports));
+})(typeof window === 'undefined' ? module.exports : window);
