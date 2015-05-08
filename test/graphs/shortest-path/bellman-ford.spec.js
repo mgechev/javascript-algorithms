d0a1358dbc2c4553cf718a596126b2ef4de8c24a
mgechev <mgechev@gmail.com>
2015-05-07 17:41:51 -0700

    Add basic tests for bellman ford

---
+++ test/graphs/shortest-path/bellman-ford.spec.js
@@ -0,0 +1,33 @@
+var exported =
+  require('../../../src/graphs/shortest-path/bellman-ford'),
+  bellmanFord = exported.bellmanFord,
+  Vertex = exported.Vertex,
+  Edge = exported.Edge;
+
+describe('Bellman-Ford', function () {
+  'use strict';
+  it('should exports a method called bellmanFord', function () {
+    expect(typeof bellmanFord).toBe('function');
+  });
+
+  it('should work for an empty graph', function () {
+    var vs = [];
+    var e = [];
+    expect(bellmanFord(vs, e, undefined))
+      .toEqual({ parents: {}, distances: { undefined: 0 } });
+  });
+
+  it('should work for a graph with a single vertex', function () {
+    var vs = [new Vertex(1)];
+    var e = [];
+    expect(bellmanFord(vs, e, 1))
+      .toEqual({ parents: { '1': null }, distances: { '1': 0 }});
+  });
+
+  it('should work in the general case', function () {
+    var vs = [new Vertex(1), new Vertex(2), new Vertex(3)];
+    var e = [];
+    expect(bellmanFord(vs, e, 1))
+      .toEqual({ parents: { '1': null }, distances: { '1': 0 }});
+  });
+});
