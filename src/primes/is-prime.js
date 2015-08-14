51228e073559a5fcd2fdacf2c8ed0d6d67172451
Robert Eisele <robert@xarg.org>
2015-08-14 14:08:43 +0200

    Improved prime factor check

--- src/primes/is-prime.js
+++ src/primes/is-prime.js
@@ -1,61 +1,44 @@
 (function (exports) {
-  'use strict';
+    'use strict';
 
-  /**
-   * Advanced (optimised) method for checking if provided number is prime.
-   * For example for number 104743 it should return true, for 104744 - false.
-   *
-   * @module primes/is-prime
-   * @param {Number} number - Number that we check on prime.
-   * @returns {Boolean} Will return true if provided number is prime.
-   *
-   * @example
-   * var isPrime = require('path-to-algorithms/src/is-prime').isPrime;
-   *
-   * console.log(isPrime(7)); // true
-   * console.log(isPrime(18)); // false
-   */
-  exports.isPrime = function (number) {
-    if (number === 1) {
-      return false;
+    /**
+     * Advanced (optimised) method for checking if provided number is prime.
+     * For example for number 104743 it should return true, for 104744 - false.
+     *
+     * @module primes/is-prime
+     * @param {Number} number - Number that we check on prime.
+     * @returns {Boolean} Will return true if provided number is prime.
+     *
+     * @example
+     * var isPrime = require('path-to-algorithms/src/is-prime').isPrime;
+     *
+     * console.log(isPrime(7)); // true
+     * console.log(isPrime(18)); // false
+     */
+    exports.isPrime = function (number) {
 
-    } else if (number < 4) {
-      /**
-       * 2 and 3 are prime
-       */
-      return true;
+        if (number < 2)
+            return false;
 
-    } else if (number % 2 === 0) {
-      return false;
+        if (number % 2 === 0)
+            return (number === 2);
+        if (number % 3 === 0)
+            return (number === 3);
 
-    } else if (number < 9) {
-      /**
-       * We have already excluded 4,6 and 8
-       */
-      return true;
+        var horizon = Math.floor(Math.sqrt(number));
+        var factor = 5;
 
-    } else if (number % 3 === 0) {
-      return false;
+        while (factor <= horizon) {
 
-    } else {
-      /**
-       * 'number' rounded to the greatest integer 'rounded' so that:
-       * rounded * rounded <= number
-       */
-      var rounded = Math.floor(Math.sqrt(number));
-      var factor = 5;
-      while (factor <= rounded) {
-        if (number % factor === 0) {
-          return false;
+            if (number % factor === 0)
+                return false;
+
+            if (number % (factor + 2) === 0)
+                return false;
+
+            factor += 6;
         }
-        if (number % (factor + 2) === 0) {
-          return false;
-        }
-        factor += 6;
-      }
-    }
-
-    return true;
-  };
+        return true;
+    };
 
 }(typeof exports === 'undefined' ? window : exports));
