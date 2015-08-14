50663769a7481ca082b3310bc9323d230b87edae
Robert Eisele <robert@xarg.org>
2015-08-14 17:01:19 +0200

    Mess with linter

--- src/primes/is-prime.js
+++ src/primes/is-prime.js
@@ -1,44 +1,49 @@
 (function (exports) {
-    'use strict';
+  'use strict';
 
-    /**
-     * Advanced (optimised) method for checking if provided number is prime.
-     * For example for number 104743 it should return true, for 104744 - false.
-     *
-     * @module primes/is-prime
-     * @param {Number} number - Number that we check on prime.
-     * @returns {Boolean} Will return true if provided number is prime.
-     *
-     * @example
-     * var isPrime = require('path-to-algorithms/src/is-prime').isPrime;
-     *
-     * console.log(isPrime(7)); // true
-     * console.log(isPrime(18)); // false
-     */
-    exports.isPrime = function (number) {
+  /**
+   * Advanced (optimised) method for checking if provided number is prime.
+   * For example for number 104743 it should return true, for 104744 - false.
+   *
+   * @module primes/is-prime
+   * @param {Number} number - Number that we check on prime.
+   * @returns {Boolean} Will return true if provided number is prime.
+   *
+   * @example
+   * var isPrime = require('path-to-algorithms/src/is-prime').isPrime;
+   *
+   * console.log(isPrime(7)); // true
+   * console.log(isPrime(18)); // false
+   */
+  exports.isPrime = function (number) {
 
-        if (number < 2)
-            return false;
+    if (number < 2) {
+      return false;
+    }
 
-        if (number % 2 === 0)
-            return (number === 2);
-        if (number % 3 === 0)
-            return (number === 3);
+    if (number % 2 === 0) {
+      return (number === 2);
+    }
 
-        var horizon = Math.floor(Math.sqrt(number));
-        var factor = 5;
+    if (number % 3 === 0) {
+      return (number === 3);
+    }
 
-        while (factor <= horizon) {
+    var horizon = Math.floor(Math.sqrt(number));
+    var factor = 5;
 
-            if (number % factor === 0)
-                return false;
+    while (factor <= horizon) {
 
-            if (number % (factor + 2) === 0)
-                return false;
+      if (number % factor === 0) {
+        return false;
+      }
 
-            factor += 6;
-        }
-        return true;
-    };
+      if (number % (factor + 2) === 0) {
+        return false;
+      }
+      factor += 6;
+    }
+    return true;
+  };
 
 }(typeof exports === 'undefined' ? window : exports));
