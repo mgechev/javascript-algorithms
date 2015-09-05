b5351887f9858f39da3fefc81dcede5cbad4a642
deniskyashif <denis.kyashif@gmail.com>
2015-09-05 11:36:52 +0300

    added tests for levenshtein distance algorithm

---
+++ test/others/levenshtein-distance.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var mod = require('../../src/others/levenshtein-distance.js');
+var levenshteinDistance = mod.levenshteinDistance;
+
+describe('Levenstein\'s minimum edit distance algorithm', function () {
+  it('"" -> "" should return 0.', function () {
+    expect(levenshteinDistance('', '')).toBe(0);
+  });
+
+  it('"T" -> "" should return 1.', function () {
+    expect(levenshteinDistance('T', '')).toBe(1);
+  });
+
+  it('"cake" -> "rake" should return 1.', function () {
+    expect(levenshteinDistance('cake', 'rake')).toBe(1);
+  });
+
+  it('"Sofia" -> "Sof" should return 2.', function () {
+    expect(levenshteinDistance('Sofia', 'Sof')).toBe(2);
+  });
+
+  it('"google" -> "lookat" should return 4.', function () {
+    expect(levenshteinDistance('google', 'lookat')).toBe(4);
+  });
+
+  it('"emacs" -> "vim" should return 5.', function () {
+    expect(levenshteinDistance('emacs', 'vim')).toBe(5);
+  });
+
+  it('"coffee" -> "cocoa" should return 4.', function () {
+    expect(levenshteinDistance('coffee', 'cocoa')).toBe(4);
+  });
+
+  it('"Munich" -> "Muenchen" should return 4.', function () {
+    expect(levenshteinDistance('Munich', 'Muenchen')).toBe(4);
+  });
+
+  it('"rosebud" -> "budrose" should return 6.', function () {
+    expect(levenshteinDistance('rosebud', 'budrose')).toBe(6);
+  });
+});
