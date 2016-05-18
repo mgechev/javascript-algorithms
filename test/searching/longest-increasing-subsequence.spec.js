b21b5e5dbfe5b010b5371ef9080b39e82ad58866
mgechev <mgechev@gmail.com>
2016-05-18 11:18:50 +0300

    test(longestIncreasingSubsequence): update tests

--- test/searching/longest-increasing-subsequence.spec.js
+++ test/searching/longest-increasing-subsequence.spec.js
@@ -12,6 +12,15 @@ describe('longest subsequence', function () {
     sequence = [5, 2, 8, 6, 3, 6, 9, 7, 11];
   });
 
+  it('should work with empty array', function () {
+    expect(longestSubsequence([]).length).toBe(0);
+  });
+
+  it('should return the only element in a single element array', function () {
+    let array = [1];
+    expect(longestSubsequence(array)).toEqual([1]);
+  });
+
   it('should give the right length', function () {
     expect(longestSubsequence(sequence).length).toBe(5);
   });
