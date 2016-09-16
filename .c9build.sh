a5de229f765b52668dfd4e6cd1999373f9926950
Mark Underseth <marku@s2technologies.com>
2016-09-16 23:07:38 +0000

    client 1.4 updates:

--- .c9build.sh
+++ .c9build.sh
@@ -11,4 +11,4 @@ gulp build
 
 # Publish
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-CI_REPORTS=$PWD/test/reports testspace publish @.testspace master.c9
+CI_REPORTS=$PWD/test/reports testspace @.testspace master.c9
