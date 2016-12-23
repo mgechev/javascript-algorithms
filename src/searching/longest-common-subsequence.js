57989d5a010610338ce5cdfc3aca0504ebd1f08b
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2016-12-23 21:40:24 +0100

    Add docs for longest common subsequence algorithm

--- src/searching/longest-common-subsequence.js
+++ src/searching/longest-common-subsequence.js
@@ -3,6 +3,19 @@
 
   exports.longestCommonSubsequence = (function () {
 
+    /**
+     * Find the lengths of longest common sub-sequences
+     * of two strings and their substrings.
+     *
+     * Complexity: O(MN).
+     *
+     * @private
+     * @param {String} first string
+     * @param {String} second string
+     * @return {Array} two dimensional array with LCS
+     * lengths of input strings and their substrings.
+     *
+     */
     function getLcsLengths(str1, str2) {
       var result = [];
       for (var i = -1; i < str1.length; i = i + 1) {
@@ -20,6 +33,19 @@
       return result;
     }
 
+    /**
+     * Find longest common sub-sequences of two strings.
+     *
+     * Complexity: O(M + N).
+     *
+     * @private
+     * @param {String} first string
+     * @param {String} second string
+     * @return {Array} two dimensional array with LCS
+     * lengths of input strings and their substrings
+     * returned from 'getLcsLengths' function.
+     *
+     */
     function getLcs(str1, str2, lcsLengthsMatrix) {
       var execute = function (i, j) {
         if (!lcsLengthsMatrix[i][j]) {
@@ -35,6 +61,22 @@
       return execute(str1.length - 1, str2.length - 1);
     }
 
+    /**
+     * Algorithm from dynamic programming. It finds the longest
+     * common sub-sequence of two strings. For example for strings 'abcd'
+     * and 'axxcda' the longest common sub-sequence is 'acd'.
+     *
+     * @example
+     * var subsequence = require('path-to-algorithms/src/searching/'+
+     * 'longest-common-subsequence').longestCommonSubsequence;
+     * console.log(subsequence('abcd', 'axxcda'); // 'acd'
+     *
+     * @public
+     * @module searching/longest-common-subsequence
+     * @param {String} first input string.
+     * @param {String} second input string.
+     * @return {Array} Longest common subsequence.
+     */
     return function (str1, str2) {
       var lcsLengthsMatrix = getLcsLengths(str1, str2);
       return getLcs(str1, str2, lcsLengthsMatrix);
