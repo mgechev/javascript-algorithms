e2abe6b898c81359daf8752f17c5dc85aa9b7efb
Jett Calleja <jettcalleja@gmail.com>
2017-05-12 18:23:39 +0800

    add Minkowski Distance

---
+++ test/others/minkowski-distance.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var mod = require('../../src/others/minkowski-distance.js');
+var minkowskiDistance = mod.minkowskiDistance;
+
+describe('Minkowski Distance', function () {
+  it('should return 1 with points (0, 1), (1, 1) in order 1.', function () {
+    expect(minkowskiDistance([0, 1], [1, 1], 1)).toBe(1);
+  });
+  it('should return 2 with points (0, 1), (1, 1) in order 2.', function () {
+    expect(minkowskiDistance([0, 1], [1, 1], 2)).toBe(1);
+  });
+  it('should return 2 with points (0, 1, 4), (1, 1, 6) in order Positive Infinity.', function () {
+    expect(minkowskiDistance([0, 1, 4], [1, 1, 6], Number.POSITIVE_INFINITY)).toBe(2);
+  });
+  it('should return 0 with points (0, 1, 4), (1, 1, 6) in order Negative Infinity.', function () {
+    expect(minkowskiDistance([0, 1, 4], [1, 1, 6], Number.NEGATIVE_INFINITY)).toBe(0);
+  });
+  it('should return 8.372966759705923 with points (0, 3, 4, 5), (7, 6, 3, -1) in order 3.', function () {
+    expect(minkowskiDistance([0, 1, 4], [1, 1, 6], Number.NEGATIVE_INFINITY)).toBe(0);
+  });
+  it('should throw when 2 points are not in equal length', function () {
+    expect( function () {
+      minkowskiDistance([1, 2], [1], 1)
+    }).toThrow('2 points must have same array length');
+  });
+  it('should throw when p is not defined', function () {
+    expect( function () {
+      minkowskiDistance([1, 2], [1, 2])
+    }).toThrow('p must be a number');
+  });
+  it('should throw when p is not a number', function () {
+    expect( function () {
+      minkowskiDistance([1, 2], [1, 2], NaN)
+    }).toThrow('p must be a number');
+  });
+  it('should throw when p is less than 1', function () {
+    expect( function () {
+      minkowskiDistance([1, 2], [1, 2], 0)
+    }).toThrow('p less than 1 will violate triangle inequality');
+  });
+});
