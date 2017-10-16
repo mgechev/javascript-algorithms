e697885ae4bef27b9da11f776561ee014ec3c49b
Arthur Guiot <arguiot@gmail.com>
2017-10-16 16:01:47 +0800

    Added average

---
+++ src/others/average.js
@@ -0,0 +1,26 @@
+/**
+ * Returns the average of a list
+ *
+ * @public
+ *
+ * @example
+ * var average = require('path-to-algorithms/src/others/average').average;
+ * var list = [477, 360, 869, 334, 541]
+ *
+ * console.log(average(list)); // 516.2
+ *
+ * @param {list} the list that you want the average of.
+ *
+ * @module others/average
+*/
+(function (exports) {
+  'use strict';
+
+  average(list) {
+    const summed = this.sum(array);
+    const average = summed / array.length;
+    return average;
+  }
+
+  exports.average = average;
+})(typeof window === 'undefined' ? module.exports : window);
