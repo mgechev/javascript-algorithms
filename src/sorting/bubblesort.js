fde6fd8058edcb24a27d62a37b8df90217c4610b
Niraj <niraj38re@gmail.com>
2019-11-08 21:22:25 +0530

    Optimized Bubble sort (#163)

--- src/sorting/bubblesort.js
+++ src/sorting/bubblesort.js
@@ -25,13 +25,18 @@
   function bubbleSort(array, cmp) {
     cmp = cmp || comparator;
     var temp;
-    for (var i = 0; i < array.length; i += 1) {
-      for (var j = i; j > 0; j -= 1) {
-        if (cmp(array[j], array[j - 1]) < 0) {
+    for (var i = 0; i < array.length - 1 ; i += 1) {
+      var swapCount = 0;
+      for (var j = 0; j < array.length - 1 - i; j += 1) {
+        if (cmp(array[j], array[j + 1 ]) > 0) {
           temp = array[j];
-          array[j] = array[j - 1];
-          array[j - 1] = temp;
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          swapCount += 1;
         }
+      }
+      if (swapCount === 0){
+        break;
       }
     }
     return array;
