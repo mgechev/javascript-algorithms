a6e4eb145e9e2a8cb270f4f7c35ad2655e2bec76
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2020-04-26 15:10:07 +0200

    simple game tests added

--- test/others/minimax.spec.js
+++ test/others/minimax.spec.js
@@ -1,80 +1,143 @@
 const minimaxBuilder = require('../../src/others/minimax.js').minimaxBuilder;
 
-describe('Minimax with tic tac toe', function () {
+describe('Minimax', function () {
   'use strict';
 
-  let game = ticTacToe();
-
-  function getAllNextStates(state) {
-    const possibleMoves = game.emptyCells(state);
-
-    return possibleMoves.map(move => ({
-      move,
-      state: game.nextState(state, move),
-    }));
-  }
-
-  const minimaxForX = minimaxBuilder(
-    getAllNextStates,
-    state => game.isGameOver(state),
-    state => game.getScore(state).x - game.getScore(state).o
-  )
-
-  const minimaxForO = minimaxBuilder(
-    getAllNextStates,
-    state => game.isGameOver(state),
-    state => game.getScore(state).o - game.getScore(state).x
-  )
-
-  it('should be defined', function () {
+  it('builder should be defined', function () {
     expect(minimaxBuilder).toBeDefined();
   });
 
-  it('should win versus dumb agent as first player', function () {
-    let state = game.newState('x');
+  describe('with tic tac toe', function () {
+    let game = ticTacToe();
 
-    while (!game.isGameOver(state)) {
-      if (state.turn === 'x') {
-        state = game.nextState(state, minimaxForX(state, true, 5, -Infinity, Infinity).move);
-      } else {
-        const move = game.emptyCells(state)[0];
-        state = game.nextState(state, move);
-      }
+    function getAllNextStates(state) {
+      const possibleMoves = game.emptyCells(state);
+
+      return possibleMoves.map(move => ({
+        move,
+        state: game.nextState(state, move),
+      }));
     }
 
-    expect(game.isGameOver(state)).toBe(true);
-    expect(game.getScore(state)).toEqual({x: 1, o: 0});
+    const minimaxForX = minimaxBuilder(
+      getAllNextStates,
+      state => game.isGameOver(state),
+      state => game.getScore(state).x - game.getScore(state).o
+    )
+
+    const minimaxForO = minimaxBuilder(
+      getAllNextStates,
+      state => game.isGameOver(state),
+      state => game.getScore(state).o - game.getScore(state).x
+    )
+
+    it('should win versus dumb agent as first player', function () {
+      let state = game.newState('x');
+
+      while (!game.isGameOver(state)) {
+        if (state.turn === 'x') {
+          state = game.nextState(state, minimaxForX(state, true, 5, -Infinity, Infinity).move);
+        } else {
+          const move = game.emptyCells(state)[0];
+          state = game.nextState(state, move);
+        }
+      }
+
+      expect(game.isGameOver(state)).toBe(true);
+      expect(game.getScore(state)).toEqual({x: 1, o: 0});
+    });
+
+    it('should win versus dumb agent as second player', function () {
+      let state = game.newState('x');
+
+      while (!game.isGameOver(state)) {
+        if (state.turn === 'o') {
+          state = game.nextState(state, minimaxForO(state, true, 5, -Infinity, Infinity).move);
+        } else {
+          const move = game.emptyCells(state)[0];
+          state = game.nextState(state, move);
+        }
+      }
+
+      expect(game.isGameOver(state)).toBe(true);
+      expect(game.getScore(state)).toEqual({x: 0, o: 1});
+    });
+
+
+    it('should be a tie for two minimax agents', function () {
+      let state = game.newState('x');
+
+      while (!game.isGameOver(state)) {
+        if (state.turn === 'o') {
+          state = game.nextState(state, minimaxForO(state, true, 5, -Infinity, Infinity).move);
+        } else {
+          state = game.nextState(state, minimaxForX(state, true, 5, -Infinity, Infinity).move);
+        }
+      }
+      expect(game.isGameOver(state)).toBe(true);
+      expect(game.getScore(state)).toEqual({x: 0, o: 0});
+    });
   });
 
-  it('should win versus dumb agent as second player', function () {
-    let state = game.newState('x');
+  describe('with simple game', function () {
+    let game = simpleGame();
 
-    while (!game.isGameOver(state)) {
-      if (state.turn === 'o') {
-        state = game.nextState(state, minimaxForO(state, true, 5, -Infinity, Infinity).move);
-      } else {
-        const move = game.emptyCells(state)[0];
-        state = game.nextState(state, move);
+    const minimaxForA = minimaxBuilder(
+      state => [true, false].map(move => ({ move, state: game.nextState(state, move)})),
+      state => game.isGameOver(state),
+      state => game.getScore(state).A - game.getScore(state).B
+    );
+    const minimaxForB = minimaxBuilder(
+      state => [true, false].map(move => ({ move, state: game.nextState(state, move)})),
+      state => game.isGameOver(state),
+      state => game.getScore(state).B - game.getScore(state).A
+    );
+
+    it('should win versus dumb agent as a first player', function () {
+      /*         o
+              /     \
+             o        o
+          /    \     /  \
+         o     o    o    o
+        / \   / \  / \   / \
+      -1   1 1  1  1 -1  1   -1
+      */
+      const binaryTree = [0, 0, 0, 0, 0, 0, 0, -1, 1, 1, 1, 1, -1, 1, -1];
+      let state = game.newState(binaryTree);
+
+      while (!game.isGameOver(state)) {
+        if (state.turn === 'A') {
+          state = game.nextState(state, minimaxForA(state, true, 5, -Infinity, Infinity).move);
+        } else {
+          state = game.nextState(state, false);
+        }
       }
-    }
 
-    expect(game.isGameOver(state)).toBe(true);
-    expect(game.getScore(state)).toEqual({x: 0, o: 1});
-  });
+      expect(game.isGameOver(state)).toBe(true);
+      expect(game.getScore(state)).toEqual({A: 1, B: -1});
+    });
 
+    it('should win versus dumb agent as a second player', function () {
+      /*     o
+           /    \
+          o      o
+         /  \   /  \
+        -1  -1 -1   1
+       */
+      const binaryTree = [0, 0, 0, -1, -1, -1, 1];
+      let state = game.newState(binaryTree);
 
-  it('should be a tie for two minimax agents', function () {
-    let state = game.newState('x');
-
-    while (!game.isGameOver(state)) {
-      if (state.turn === 'o') {
-        state = game.nextState(state, minimaxForO(state, true, 5, -Infinity, Infinity).move);
-      } else {
-        state = game.nextState(state, minimaxForX(state, true, 5, -Infinity, Infinity).move);
+      while (!game.isGameOver(state)) {
+        if (state.turn === 'B') {
+          state = game.nextState(state, minimaxForB(state, true, 5, -Infinity, Infinity).move);
+        } else {
+          state = game.nextState(state, false);
+        }
       }
-    }
-    expect(game.isGameOver(state)).toBe(true);
-    expect(game.getScore(state)).toEqual({x: 0, o: 0});
+
+      expect(game.isGameOver(state)).toBe(true);
+      expect(game.getScore(state)).toEqual({A: -1, B: 1});
+    });
   });
 });
 
@@ -162,5 +225,53 @@ function ticTacToe() {
     nextState,
     isGameOver,
     emptyCells,
+  }
+}
+
+
+/* A simple game made for the purpose of minimax testing. The game has a binary tree with end values: 1 for  player A win and -1 for player B win.
+ Game starts from the root node and each player has a binary choose - "false" moves to the left child and "true" moves to the right child.
+ The game ends when the very bottom leaf is reached.
+       o
+     /    \
+    o      o
+   /  \   /  \
+  1  -1 -1   -1
+ */
+function simpleGame() {
+  'use strict';
+
+  function newState(binaryTree) {
+    return {
+      turn: 'A',
+      tree: binaryTree,
+      position: 0,
+    };
+  }
+
+  function nextState(state, move) {
+    return {
+      tree: state.tree,
+      position: move ? state.position * 2 + 2 : state.position * 2 + 1,
+      turn: state.turn === 'A' ? 'B' : 'A',
+    };
+  }
+
+  function isGameOver(state) {
+    return state.tree[state.position] !== 0;
+  }
+
+  function getScore(state) {
+    return {
+      A: state.tree[state.position],
+      B: state.tree[state.position] === 0 ? 0 : -state.tree[state.position],
+    }
+  }
+
+  return {
+    newState,
+    nextState,
+    isGameOver,
+    getScore,
   }
 }
