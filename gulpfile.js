c4e35f2e43ea846b24529918011e8d4ab95dfe3b
Daniel Wasserlauf <dwasserlauf@commercehub.com>
2020-10-14 17:28:24 -0400

    [gulpUpdate]: Updated Gulp dependency to fix build failure

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-'use strict';
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var jasmine = require('gulp-jasmine');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const jasmine = require('gulp-jasmine');
 
-gulp.task('test', function () {
+gulp.task('test', () => {
+  'use strict';
   return gulp.src('test/**/*.spec.js')
     .pipe(jasmine());
 });
 
-gulp.task('lint', function () {
+gulp.task('lint', ()=> {
+  'use strict';
   return gulp.src(['src/**/*.js', 'test/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('build', ['lint', 'test']);
+gulp.task('build', gulp.parallel(['lint', 'test']));
