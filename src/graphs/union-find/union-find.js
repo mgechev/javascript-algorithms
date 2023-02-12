e4e7deea3a076d17171778f2398f886c9c0654c7
ramzi <ramziinfo2001@gmail.com>
2023-02-12 10:21:29 +0100

    adding union find data structure

---
+++ src/graphs/union-find/union-find.js
@@ -0,0 +1,32 @@
+const unionFind = (edges) => {
+    let parent = [];
+
+    for (let i = 0; i <= edges.length; i++) {
+        parent.push(i);
+    }
+
+    // finding the parent of p
+    const find = (p) => {
+        if (parent[p] === p) {
+            return p;
+        } else {
+            return find(parent[p]);
+        }
+    };
+
+    const union = (u, v) => {
+        let parentU = find(u);
+        let parentV = find(v);
+
+        // u becomes the parent of v ;
+        parent[parentU] = parentV;
+    };
+};
+
+unionFind([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [1, 4],
+    [1, 5],
+]);
