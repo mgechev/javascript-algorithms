51c808090d2a2f80eda9d0e838caa071d768b7f4
Alok <alokshivsingh@gmail.com>
2023-10-13 18:58:36 +0530

    updated the binary search from es5 to es6

--- src/searching/binarysearch.js
+++ src/searching/binarysearch.js
@@ -1,9 +1,8 @@
 (function (exports) {
   'use strict';
 
-  function id (val) { return val; }
-  function get (key) { return function (val) { return val[key]; }; }
-
+  const id = (val) => val;
+  const get = (key) => (val) => val[key];
   /**
    * Searches for specific element in a given array using
    * the binary search algorithm.<br><br>
@@ -21,14 +20,14 @@
    * @param {Number} value Value of the element which index should be found.
    * @returns {Number} Index of the element or -1 if not found.
    */
-  function binarySearch(array, value, key) {
+  const binarySearch = (array, value, key) => {
     key = !key ? id : typeof key === 'string' ? get(key) : key;
     value = key(value);
-    var middle = Math.floor(array.length / 2);
-    var left = 0;
-    var right = array.length;
+    let middle = Math.floor(array.length / 2);
+    let left = 0;
+    let right = array.length;
     while (right >= left) {
-      var middleValue = key(array[middle]);
+      const middleValue = key(array[middle]);
       if (middleValue === value) {
         return middle;
       } else if (middleValue > value) {
@@ -39,8 +38,7 @@
       middle = Math.floor((left + right) / 2);
     }
     return -1;
-  }
-
+  };
   exports.binarySearch = binarySearch;
 
 })(typeof window === 'undefined' ? module.exports : window);
